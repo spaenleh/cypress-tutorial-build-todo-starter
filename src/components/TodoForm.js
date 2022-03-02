518dc35abdd5ec7081cef0021d5376a20caa029c
spaenleh <basile.1358@gmail.com>
2022-03-02 17:42:53 +0100

    feat: finish walktrough of cypress tutorial

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export default props =>
-  <form>
+  <form onSubmit={props.handleTodoSubmit}>
     <input
       type='text'
+      value={props.currentTodo}
+      onChange={props.handleNewTodoChange}
+      autoFocus
       className="new-todo"
       placeholder="What needs to be done?"/>
   </form>
